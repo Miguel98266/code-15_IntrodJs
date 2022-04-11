a7b2cb8d63466d7e4a76283de53e2b6cc4833daa
Miguel98266 <35313138+Miguel98266@users.noreply.github.com>
2022-04-10 23:44:59 -0500

    Avance Loteria

--- 11_Proyecto_Loteria/js/app.js
+++ 11_Proyecto_Loteria/js/app.js
@@ -1,32 +1,36 @@
 import { tarjetas } from "./tarjetas.js";
 import { tablero1, tablero2, tablero3 } from "./barajear.js";
-// import { jugador1 } from './appindex.js';
 
 console.log(tablero1);
+// Variables a utilizar
 let boton = document.getElementById("carta");
 let cardImage = document.querySelector(".card-img-top");
-let cardTitle = document.querySelector(".card-title");
+// let cardTitle = document.querySelector(".card-title");
 let btninicio = document.querySelector("#inicio");
 let card = document.querySelector(".card");
-let tab1 = document.querySelector("#tabl-1");
 let tarjetaspasadas = [];
-card.setAttribute("class", "visually-hidden");
-
 let tableroj1 = document.querySelectorAll(".tableroj1");
 let tableroj2 = document.querySelectorAll(".tableroj2");
-// card.style="display:none";
 let jugado1 = document.getElementsByName("jugador1");
 let jugado2 = document.getElementsByName("jugador2");
+let ganador=document.getElementById('ganador');
+let tarjetaspasda=document.getElementById('tarjetaspasda');
 
-// console.log(document.querySelectorAll('[name="jugador1"]'));
+// Esconde la Card donde se muestran las cartas
+card.setAttribute("class", "visually-hidden");
 
-// let ju1=document.querySelectorAll('[name="jugador1"]')
+// Carga los tableros seleccionados por los jugadores y valida si mandaron algo vacio 
 window.addEventListener("load", () => {
   const params = new URL(document.location).searchParams;
+  const numparams= Array.from(params).length;
+  console.log(numparams);
   const jugador1 = params.get("jugador1");
   const jugador2 = params.get("jugador2");
   console.log(jugador1, jugador2);
-
+  if (jugador1 == null || jugador2 == null) {
+    alert("Falto seleccionar un tablero , seleccionalo");
+    window.location.replace("http://127.0.0.1:5500/index.html");
+  }
   if (jugador1 == "tablero1") {
     for (let i = 0; i < 16; i++) {
       tableroj1[i].setAttribute("src", tablero1[i].img);
@@ -43,7 +47,7 @@ window.addEventListener("load", () => {
     for (let i = 0; i < 16; i++) {
       tableroj1[i].setAttribute("src", tablero3[i].img);
       tableroj1[i].setAttribute("alt", tablero3[i].nombre);
-      jugado1[i].setAttribute("value", tablero1[i].nombre);
+      jugado1[i].setAttribute("value", tablero3[i].nombre);
     }
   }
 
@@ -67,32 +71,28 @@ window.addEventListener("load", () => {
     }
   }
 });
-console.log(document.getElementsByName("jugador1").checked);
 
-// console.log(jugado1)
+// Ejecucion de Validacion de la Carta 1
 jugado1.forEach(function (jugador) {
   jugador.addEventListener("change", validarCartaJugador1);
-  console.log("Funciona?");
+  // console.log("Funciona?");
 });
+// Ejecucion de Validacion de la Carta 2
 jugado2.forEach(function (jugador) {
   jugador.addEventListener("change", validarCartaJugador2);
-  console.log("Funciona?");
+  // console.log("Funciona?");
 });
 
+console.log(jugado1)
+
 function validarCartaJugador1() {
-  // const result = jugado1.filter(jugado1 => jugado1.disabled);
-  // console.log(result);
-  // jugador1 = document.getElementsByName("jugador1");
-  // console.log(jugador1);
   for (let e = 0; e < jugado1.length; e++) {
-    // let prueba1 = document.querySelectorAll('[name="jugador1"]:checked')
     
-    if (jugado1[e].checked == true) {
+    if (jugado1[e].checked == true && !jugado1[e].disabled) {
+      console.log(jugado1[e]);
       console.log("Si esta check");
-      // console.log(tarjetaspasadas[tarjetaspasadas.length-1].nombre)
-      // console.log(jugador1[e].value)
       if (
-        jugado1[e].value == tarjetaspasadas[tarjetaspasadas.length - 1].nombre
+        jugado1[e].value == tarjetaspasadas[tarjetaspasadas.length - 1]
       ) {
         console.log("Hizo Match");
         let div_ju = document.querySelectorAll(".div-ju");
@@ -102,24 +102,32 @@ function validarCartaJugador1() {
         frijol.style =
           "position: absolute;top: 50%; left: 50%;transform: translate(-50%, -50%); ";
         div_ju[e].appendChild(frijol);
-        // alert("Felicidades llevas otra carta mas");
         jugado1[e].setAttribute("disabled", "");
-      } else {
-        // jugado1[e].checked = 0;
-        // alert("La tarjeta marcada no hace match con la baraja");
+      }
+      else if(tarjetaspasadas.find(elemento=>elemento.nombre==jugado1[e].value)){
+        console.log("Hizo Match con una tarjeta pasada");
+        let div_ju = document.querySelectorAll(".div-ju");
+        let frijol = document.createElement("img");
+        frijol.className = "frijol";
+        frijol.src = "img/frijol_oscuro.png";
+        frijol.style =
+          "position: absolute;top: 50%; left: 50%;transform: translate(-50%, -50%); ";
+        div_ju[e].appendChild(frijol);
+        jugado1[e].setAttribute("disabled", "");
+      } 
+      else {
+        jugado1[e].checked = 0;
+        alert("La tarjeta marcada no hace match con la baraja");
       }
     }
   }
 }
+// Valida la carta para poner el frijolito del jugador 2
 function validarCartaJugador2() {
-  // jugador1 = document.getElementsByName("jugador1");
-  // console.log(jugador1);
   for (let e = 0; e < jugado2.length; e++) {
-    console.log(jugado2[0]);
+    // console.log(jugado2[0]);
     if (jugado2[e].checked == true) {
-      console.log("Si esta check");
-      // console.log(tarjetaspasadas[tarjetaspasadas.length-1].nombre)
-      // console.log(jugador1[e].value)
+      // console.log("Si esta check");
       if (
         jugado2[e].value == tarjetaspasadas[tarjetaspasadas.length - 1].nombre
       ) {
@@ -131,49 +139,69 @@ function validarCartaJugador2() {
         frijol.style =
           "position: absolute;top: 50%; left: 50%;transform: translate(-50%, -50%); ";
         div_ju[e].appendChild(frijol);
-        // alert("Felicidades llevas otra carta mas");
         jugado2[e].setAttribute("disabled", "");
       }
-      // else{
-      //   alert("La tarjeta marcada no hace match con la baraja")
-      // }
+      else{
+        alert("La tarjeta marcada no hace match con la baraja")
+      }
+
+
     }
   }
 }
 
-boton.addEventListener("click", function () {
+boton.addEventListener("click", sigTarjeta);
+function sigTarjeta() {
   if (tarjetas.length == 0) {
     alert("El juego ha terminado");
+    window.location.replace("http://127.0.0.1:5500/index.html");
+
   } else {
     var obj = tarjetas.shift();
     console.log(obj.nombre);
     cardImage.setAttribute("src", obj.img);
-    cardTitle.textContent = obj.nombre;
+    speechSynthesis.speak(new SpeechSynthesisUtterance(obj.nombre));
+    // cardTitle.textContent = obj.nombre;
     tarjetaspasadas.push(obj);
     console.log(tarjetaspasadas);
+    let obj2=document.createElement('img');
+    obj2.src=obj.img;
+    obj2.alt=obj.nombre;
+    obj2.className="height";
+    tarjetaspasda.appendChild(obj2);
   }
-});
-
+}
+// Inicio al Juego
 btninicio.addEventListener("click", function () {
   card.setAttribute("style", "width: 15rem; ");
   card.setAttribute(
     "class",
-    "card mx-auto text-center my-4 visible prueba border-0 background"
+    "card mx-auto text-center my-4 visible prueba"
   );
-  cardImage.setAttribute("src", tarjetas[0].img);
-  cardTitle.textContent = tarjetas[0].nombre;
-  tarjetaspasadas.push(tarjetas.shift());
+  // cardImage.setAttribute("src", tarjetas[0].img);
+  // cardTitle.textContent = tarjetas[0].nombre;
+  // tarjetaspasadas.push(tarjetas.shift());
+  // console.log(tarjetaspasadas);
   btninicio.remove();
+  setInterval(() => {
+    sigTarjeta();
+  }, 3000);
 });
+
+// Boton de Loteria evalua quien gana
 
 ganador.addEventListener("click", function () {
   let jugado1 = document.querySelectorAll('[name="jugador1"]:checked').length;
   console.log(jugado1);
   let jugado2 = document.querySelectorAll('[name="jugador2"]:checked').length;
   if (jugado1 == "16") {
-    $('#ganojugador1').modal('show');
-    
+    $("#ganojugador1").modal("show");
   } else if (jugado2 == "16") {
-    $('#ganojugador2').modal('show');
+    $("#ganojugador2").modal("show");
+  } else{
+    $("#faltacartas").modal("show");
   }
 });
+
+
+
--- 11_Proyecto_Loteria/js/appindex.js
+++ 11_Proyecto_Loteria/js/appindex.js
@@ -1,115 +1,87 @@
-
-import {tablero1,tablero2,tablero3} from './barajear.js'
+import { tablero1, tablero2, tablero3 } from "./barajear.js";
 // ------------Generacion de Tableros-------------------------
-console.log(tablero1);
-// let btn=document.getElementById('btnbarajear');
 
-// btn.addEventListener('click',tableros);
-// let abre=document.getElementById("");
-
-document.addEventListener('DOMContentLoaded',loadTableros);
-
+document.addEventListener("DOMContentLoaded", loadTableros);
+// Carga las imagenes de los 3 tableros a escojer
 function loadTableros() {
-  $('#exampleModal').modal('show');
-  // abre.setAttribute("data")
-  let table1 = document.querySelectorAll(".tablero1");
-  for (let i = 0; i < 16; i++) {
-    table1[i].setAttribute("src", tablero1[i].img);
-    table1[i].setAttribute("alt", tablero1[i].nombre);
-  }
-  let table2 = document.querySelectorAll(".tablero2");
-  console.log(table2.length);
-  for (let i = 0; i < 16; i++) {
-    table2[i].setAttribute("src", tablero2[i].img);
-    table2[i].setAttribute("alt", tablero2[i].nombre);
-  }
-  let table3 = document.querySelectorAll(".tablero3");
-  console.log(table3.length);
-  for (let i = 0; i < 16; i++) {
-    table3[i].setAttribute("src", tablero3[i].img);
-    table3[i].setAttribute("alt", tablero3[i].nombre);
-  }
+  $("#exampleModal").modal("show");
+  
+  let ptablero = document.querySelector("#primer_tablero");
+  cargarTablero(tablero1,ptablero);
+
+  let stablero = document.querySelector("#segundo_tablero");
+  cargarTablero(tablero2,stablero);
+  
+  let ttablero = document.querySelector("#tercer_tablero");
+  cargarTablero(tablero3,ttablero);
+  
 }
 
-
-
+export function cargarTablero(tablero, iddiv) {
+  for (const obj of tablero) {
+    // console.log(tablero);
+    let div = document.createElement("div");
+    div.className = "col-3 text-center";
+    let img = document.createElement("img");
+    img.className = "img-thumbnail";
+    img.src = obj.img;
+    img.alt = obj.nombre;
+    div.appendChild(img);
+    iddiv.appendChild(div);
+  }
+}
 export const jugador1 = document.getElementsByName("jugador1");
 export const jugador2 = document.getElementsByName("jugador2");
 
-export const tabj1=document.getElementById("tablero1j1");
-export const tab2j1=document.getElementById("tablero2j1");
-export const tab3j1=document.getElementById("tablero3j1");
+export const tabj1 = document.getElementById("tablero1j1");
+export const tab2j1 = document.getElementById("tablero2j1");
+export const tab3j1 = document.getElementById("tablero3j1");
 
-export const tabj2=document.getElementById("tablero1j2");
-export const tab2j2=document.getElementById("tablero2j2");
-export const tab3j2=document.getElementById("tablero3j2");
+export const tabj2 = document.getElementById("tablero1j2");
+export const tab2j2 = document.getElementById("tablero2j2");
+export const tab3j2 = document.getElementById("tablero3j2");
+// Revision de Tableros
+tabj1.addEventListener("click", revisarj1);
+tab2j1.addEventListener("click", revisarj1);
+tab3j1.addEventListener("click", revisarj1);
 
-tabj1.addEventListener("click",revisarj1);
-tab2j1.addEventListener("click",revisarj1);
-tab3j1.addEventListener("click",revisarj1);
-
-tabj2.addEventListener("click",revisarj2);
-tab2j2.addEventListener("click",revisarj2);
-tab3j2.addEventListener("click",revisarj2);
+tabj2.addEventListener("click", revisarj2);
+tab2j2.addEventListener("click", revisarj2);
+tab3j2.addEventListener("click", revisarj2);
 
 function revisarj1() {
   for (let i = 0; i < jugador1.length; i++) {
-    if (jugador1[i].checked == true) {
+    if (jugador1[i].checked) {
       console.log("Esta check");
       jugador2[i].setAttribute("disabled", "");
 
       console.log(jugador1[i].value);
-      var tab=jugador1[i].value}
-      else if(jugador1[i].checked == false) {
+      var tab = jugador1[i].value;
+    } else if (!jugador1[i].checked) {
       jugador2[i].removeAttribute("disabled");
     }
   }
   return tab;
-};
+}
 
 function revisarj2() {
   for (let j = 0; j < jugador2.length; j++) {
-    if (jugador2[j].checked == true) {
+    if (jugador2[j].checked) {
       console.log("Esta check");
       jugador1[j].setAttribute("disabled", "");
+
       console.log(jugador2[j].value);
-      var tab2=jugador2[j].value}
-     else {
+      var tab2 = jugador2[j].value;
+    } else if (!jugador2[j].checked) {
       jugador1[j].removeAttribute("disabled");
     }
   }
   return tab2;
-};
-
-
-// j1=revisarj1();
-// console.log("Ejecuto")
-// console.log(revisarj2())
-// let jtablero1 = document.getElementsByName("Jugador1");
-// console.log(jtablero1);
-// let j2tablero1 = document.getElementsByName("Jugador2");
-
-// console.log(j2tablero1);
-// function jugadores() {
-//   let jugadores = [];
-//   let jugador1 = [];
-//   let jugador2 = [];
-//   for (let i = 0; i < jtablero1.length; i++) {
-//     if (jtablero1[i].checked == true) {
-//       console.log("Si esta check");
-//       // console.log(tarjetaspasadas[tarjetaspasadas.length-1].nombre)
-//       // console.log(jugador1[e].value)
-//       j2tablero1[0].setAttribute("disabled", "");
-//       jtablero1[i].setAttribute("disabled", "");
-//       jugador1.push(tablero1);
-//       jugadores.push(jugador1);
-//       console.log(jugadores);
-//     } else if (j2tablero1[i].checked == true) {
-//       jtablero1[i].setAttribute("disabled", "");
-//       jugador2.push(tablero1);
-//       jugadores.push(jugador2);
-//     }
+}
+// let continuar =document.getElementById('continuar');
+// continuar.addEventListener("submit",function(){
+//   e.preventDefault();
+//   if (jugador1=='' || jugador2=='') {
+//     alert('Falta seleccionar un tablero')
 //   }
-
-//   return;
-// }
+// });
